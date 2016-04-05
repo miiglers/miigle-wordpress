2d0613d49eccb3adfe49664dbdde12f6df5d3706
Josh Fester <joshfester@gmail.com>
2016-04-05 01:24:48 -0700

    more homepage sections

--- wp-content/themes/sage/front-page.php
+++ wp-content/themes/sage/front-page.php
@@ -28,7 +28,7 @@
     <div class="container">
 
       <h2>How it Works</h2>
-      <p>blah blah blah blah lahsd h ga;lkhsegoi h lh ohw gklshg oeh asdf</p>
+      <p class="lead">blah blah blah blah lahsd h ga;lkhsegoi h lh ohw gklshg oeh asdf</p>
       
       <div class="row">
       
@@ -59,6 +59,60 @@
     <div class="container">
 
       <p>If not us, who? If not now, when? - Josh Fester</p>
+
+    </div>
+  </section>
+  
+  <section id="story">
+    <div class="container">
+
+      <h2>Our Story</h2>
+      
+      <div class="row">
+        <div class="col-md-6">
+          In 2003 I was one of six pre-med students selected nationwide by the University of Maryland School of Medicine to intern in a medical research program to develop a vaccine against Malaria in Mali, West Africa. As a naive 20 year old college student who dreamed of changing the world, that was the greatest honor of my life and a humbling lesson on Giving Back.
+
+Fast track to nine years later and a MBA in hand, I quit my six figure job, tapped into my savings and along with Josh and John co-founded Miigle, a platform that allowed people to discover and foster innovative projects around the world that matched their interests. Our goal was to empower the public to help not just innovators raising money, but also those needing emotional, practical, and intellectual support, especially in impoverished regions around the world.
+
+We launched Miigle at 2014 The Launch Festival in San Francisco, and were told by the judges on stage, Silicon Valley Investors, that we would never succeed because we weren’t part of the “Boys Club” and money gets you everything. Watch the video
+
+Did it hurt? Terribly. But we learned that we had to do better and most importantly that there are millions of people working hard every day to make this world a better place and we didn’t want to sit on the sidelines.
+
+
+<blockquote>“There are millions of people working hard every day to make this world a better place and we didn’t want to sit on the sidelines.”</blockquote>
+
+
+Miigle+ Products is a commitment to our mission to empower all of us to help the people and brands who are making a positive difference by creating socially and environmentally conscious products.
+
+This Earth Day, please join us in giving them a helping hand. Request your priority access today.
+
+
+Luc Berlin, Cofounder
+        </div>
+        <div class="col-md-6">
+          <p><img class="img-responsive" src="https://i.ytimg.com/vi/icqDxNab3Do/maxresdefault.jpg"></p>
+          <p><img class="img-responsive" src="http://www.funny-animalpictures.com/media/content/items/images/funnycats0059_O.jpg"></p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  
+  <section id="video" class="text-center">
+    <div class="container">
+
+      <h2>Together, we can change the world</h2>
+      <h3>Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum</h3>
+
+    </div>
+  </section>
+  
+  <section id="contact" class="text-center">
+    <div class="container">
+
+      <h2>Miigle+ Products is starting out in beta. We're 
+going to grant access as quickly as we can.</h2>
+      <p>To apply for access, please tell us a little bit about yourself.</p>
 
     </div>
   </section>
