966a79cca55404c512bf65a5835562e25e1102b8
Josh Fester <joshfester@gmail.com>
2016-04-09 23:22:58 -0700

    video

--- wp-content/themes/sage/front-page.php
+++ wp-content/themes/sage/front-page.php
@@ -72,5 +72,18 @@
 
     </div>
   </section>
+  
+  <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="modal-body">
+          <iframe src="https://player.vimeo.com/video/77944311" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </div>
