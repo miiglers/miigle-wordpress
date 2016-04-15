984efae386e5eb7bb78d6f7ad5e08826809acb64
joshfester <joshfester@gmail.com>
2016-04-14 18:20:36 -0700

    mobile shit

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -1,9 +1,9 @@
 <footer id="footer" class="text-center">
-  <div class="links">
-    <a href="https://www.facebook.com/miiglers/">Facebook</a>
-    <a href="https://twitter.com/miiglers">Twitter</a>
-    <a href="https://plus.google.com/+Miiglers/posts">Google+</a>
-  </div>
+  <ul class="links list-unstyled">
+    <li><a href="https://www.facebook.com/miiglers/">Facebook</a></li>
+    <li><a href="https://twitter.com/miiglers">Twitter</a></li>
+    <li><a href="https://plus.google.com/+Miiglers/posts">Google+</a></li>
+  </ul>
   <div class="copyright">
     &copy; 2016 MIIGLE
   </div>
