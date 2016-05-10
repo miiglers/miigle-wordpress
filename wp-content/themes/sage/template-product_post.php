cf4769f1d487fc5c4eeb29facbe9f12c017ee7f9
yukirosene <yuki2msg@gmail.com>
2016-05-10 14:37:05 -0700

    Added profile side bar to Product Post

--- wp-content/themes/sage/template-product_post.php
+++ wp-content/themes/sage/template-product_post.php
@@ -7,9 +7,32 @@
 <div id="template-product_post">
   
   <section id="product_post">
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-md-8 col-md-offset-2"> 
+        <div class="col-sm-3 col-md-2 sidebar" id="profile-sidebar">
+        	<div class="profile-thumb text-center">
+        		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/profile-thumb.png" /></div>
+        	<div class="profile-fullname text-center white-text">
+        		Full name</div>
+        	<div class="profile-id text-center white-text">
+        		#1</div>
+        	<div class="profile-username text-center white-text">
+        		@miigle_username</div>
+        	<div class="profile-website text-center">
+        		<a href="#" target="_blank">website.com</a></div>
+        	<div class="profile-social text-center">
+        		<a href="#" target="_blank">Twitter</a>&nbsp;|&nbsp;<a href="#" target="_blank">Facebook</a></div>
+        	<div class="portfolio-btn text-center">
+        		<a href="#" class="btn btn-profile">Go to your profile</a></div>
+        	<div class="spacer-profile"></div>
+        	<div class="profile-footer text-center">
+        		<a href="#" target="_blank">Log out</a>&nbsp;|&nbsp;
+        		<a href="#" target="_blank">Blog</a>&nbsp;|&nbsp;
+        		<a href="#" target="_blank">Contact</a>
+        	</div>
+        </div>
+        
+        <div class="col-sm-7 col-sm-offset-4 col-md-6 col-md-offset-4 main"> 
           <?php while (have_posts()) : the_post(); ?>
             <?php the_content(); ?>
           <?php endwhile; ?>
