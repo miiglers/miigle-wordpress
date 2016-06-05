c7e5a57acb61e58cf98af7b374b12177a1746f55
joshfester2 <joshfester@gmail.com>
2016-06-05 06:19:41 +0000

    instagram link

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -2,7 +2,7 @@
   <ul class="links list-unstyled">
     <li><a href="https://www.facebook.com/miiglers/">Facebook</a></li>
     <li><a href="https://twitter.com/miiglers">Twitter</a></li>
-    <li><a href="https://plus.google.com/+Miiglers/posts">Google+</a></li>
+    <li><a href="https://instagram.com/miiglers">Instagram</a></li>
   </ul>
   <div class="copyright">
     &copy; 2016 MIIGLE
