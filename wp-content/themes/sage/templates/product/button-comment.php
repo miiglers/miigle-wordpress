7cec7a1cbe76344e080d8560e3dff179120522c4
joshfester2 <joshfester@gmail.com>
2016-06-30 04:19:57 +0000

    upvote buttons

--- wp-content/themes/sage/templates/product/button-comment.php
+++ wp-content/themes/sage/templates/product/button-comment.php
@@ -3,12 +3,12 @@
   use Miigle\Models\Product;
 
   $comments = Product\get_comments_count(get_the_ID());
-  $label_class = 'label-default';
+  $label_class = '';
 
   if($comments) {
-    $label_class = 'label-pink';
+    $label_class = 'text-info';
   }
 
 ?>
 
-<span class="label label-btn <?= $label_class ?>"><i class="fa fa-commenting-o"></i> <?= $comments ?></span>
+<span class="<?= $label_class ?>"><i class="fa fa-commenting-o"></i> <?= $comments ?></span>
--- wp-content/themes/sage/templates/product/button-upvote.php
+++ wp-content/themes/sage/templates/product/button-upvote.php
@@ -4,12 +4,12 @@
   use Miigle\Models\Product;
 
   $upvoted = User\has_upvoted_product($mgl_current_user['ID'], get_the_ID());
-  $label_class = 'label-default';
+  $label_class = '';
   $i_class = 'fa-star-o';
   $endpoint = 'upvote';
 
   if($upvoted) {
-    $label_class = 'label-danger';
+    $label_class = 'text-danger';
     $i_class = 'fa-star';
     $endpoint = 'downvote';
   }
@@ -18,7 +18,7 @@
 
 <form class="upvote" id="product-upvote" method="post" action="mgl/v1/product/<?= $endpoint ?>">
   <input type="hidden" name="product_id" value="<?php the_ID(); ?>"> 
-  <span class="label label-btn btn-upvote <?= $label_class ?>" 
+  <span class="btn-upvote <?= $label_class ?>" 
   data-upvoted="<?= intval($upvoted) ?>">								
     <i class="fa <?= $i_class ?>"></i> 
     <span id="upvotes"><?= Product\get_upvotes(get_the_ID()) ?></span>
