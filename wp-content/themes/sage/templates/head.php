4fee1764041437ee6aedcaa7ad21be0fdc324554
joshfester <joshfester@gmail.com>
2016-07-15 20:27:02 -0500

    google verification

--- wp-content/themes/sage/templates/head.php
+++ wp-content/themes/sage/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="8JpOdoRK4Ckx4P8haiNQ-BLE90a9E8djBuA7D_8NkyU" />
   <?php wp_head(); ?>
 </head>
