9a6fecab03e3461e0b90f495d08974264a810ab8
joshfester <joshfester@gmail.com>
2016-07-17 18:23:21 -0500

    remove google verification and add doorbell.io code

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -53,3 +53,14 @@
   var wpHomeUrl = '<?= home_url() ?>';
   /* ]]> */
 </script>
+<script type="text/javascript">
+    window.doorbellOptions = {
+        appKey: '7M5iVcyYTkv5owR7e9lZG0efWtKsfBmNR8vBhRZGUCofIbpLAfSbAHpjlanGyf0d'
+    };
+    (function(w, d, t) {
+        var hasLoaded = false;
+        function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/4036?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+        if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+        if (d.readyState == 'complete') { l(); }
+    }(window, document, 'script'));
+</script>
