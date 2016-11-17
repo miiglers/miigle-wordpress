691b678ea78983b4a182a88c00a9b9633269d9af
yukirosene <yuki2msg@gmail.com>
2016-11-16 17:42:50 -0700

    Directory Home HTML

--- wp-content/themes/sage/template-brand_directory.php
+++ wp-content/themes/sage/template-brand_directory.php
@@ -8,12 +8,12 @@ $user = wp_get_current_user();
 
 <div id="template-brand_directory">
   
-  <section id="brand_directory" class="mT">
+  <section id="brand_directory">
 		<div class="container">
 			<div class="row">
 				
 				<!-- main section-->
-				<div id="mainCol" class="col-sm-9">
+				<div id="mainCol" class="col-lg-8 divider">
         	<?php the_field('page_heading'); ?>        	
 					
 					<div class="browseWrapper mT">
@@ -90,10 +90,25 @@ $user = wp_get_current_user();
 				<!-- /. main section-->
 				
 				<!-- side section-->
-				<div id="sideCol" class="col-sm-3">
-          	<h3>Spread The Word</h3>
-          	         	
-
+				<div id="sideCol" class="col-lg-4 divider">
+          
+          <div class="widgetBox">
+						<h3>Spread The Word</h3>
+						<p>Know a social good brand that should be here? Let them know!</p>	         	
+						<br>
+						<p><a href="#" class="btn btn-default btn-cta">EMAIL THIS PAGE</a></p>
+						<p><a href="#" class="btn btn-default btn-cta">TWEET THIS PAGE</a></p>
+						<p><a href="#" class="btn btn-default btn-cta">SHARE ON FACEBOOK</a></p>
+						<p><a href="#" class="btn btn-default btn-cta">BOOKMARK THIS PAGE</a></p>
+					</div>
+					
+					<div class="widgetBox">
+						<h3>Stay In The Know</h3>
+						<p>Crazy about Social Good like we are? Get new brands delivered straight to your inbox!</p>
+						<br>
+						<p>Enter your email</p>
+					</div>
+					
 				</div>
 				<!-- /. side section-->
 			
